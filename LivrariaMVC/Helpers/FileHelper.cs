57140eaa12e1f9c0842133e0c2aa2eb432e40648
italosanches <italo_carvalho157@hotmail.com>
2024-07-31 16:30:22 -0300

    Criado helper para trabalhar com upload de arquivo

---
+++ LivrariaMVC/Helpers/FileHelper.cs
@@ -0,0 +1,35 @@
+﻿namespace LivrariaMVC.Helpers
+{
+    public static class FileHelper
+    {
+        private static readonly string[] SupportedImageTypes = { ".jpg", ".jpeg", ".png", ".svg" };
+        public static async Task<string?> ProcessarImgUploadAsync(IFormFile FormFile, string UploadFolderPath)
+        {
+            if (FormFile == null || FormFile.Length < 0)
+            {
+                return null;
+            }
+            else
+            {
+                if(IsSupportedImageTypes(FormFile))
+                {
+                    var filePath = Path.Combine(UploadFolderPath, Path.GetFileName(FormFile.FileName));
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await FormFile.CopyToAsync(stream);
+                    }
+                    return "/uploads/" + Path.GetFileName(FormFile.FileName);
+                }
+                return "invalido"; 
+            }
+        }
+
+        private static bool IsSupportedImageTypes(IFormFile File)
+        {
+            var extension = Path.GetExtension(File.FileName).ToLowerInvariant();
+            return SupportedImageTypes.Contains(extension);
+        }
+
+    }
+}
+
