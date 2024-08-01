c1c0bb97f2756a75db31645bb0d57e55a0ca0a3e
italosanches <italo_carvalho157@hotmail.com>
2024-08-01 15:11:08 -0300

    Adicao de interface

---
+++ LivrariaMVC/Repository/Interfaces/IGeneroRepository.cs
@@ -0,0 +1,19 @@
+﻿using LivrariaMVC.Models;
+
+namespace LivrariaMVC.Repository.Interfaces
+{
+    public interface IGeneroRepository
+    {
+        IEnumerable<Genero> Generos { get; }
+
+        public Task<Genero> GetByIdAsync(int id);
+
+        public Task<Genero> GetByNameAsync(string name);
+
+        public Task<int> CreateAsync(Genero genero);
+
+        public Task<bool> UpdateAsync(Genero genero);
+
+        public Task<bool> DeleteAsync(Genero genero);
+    }
+}
